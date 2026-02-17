421332679607c2068f5a9f5336cc52142483780f
Poojachandrika2905 <pooja.chandrika2004@gmail.com>
2026-02-17 21:17:09 +0530

    Updated server.js to use environment PORT for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -5,16 +5,22 @@ require("dotenv").config();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("MongoDB Connected"))
 .catch(err=>console.log(err));
 
+// Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
-app.listen(5000, ()=>{
-console.log("Server running on port 5000");
+// PORT for deployment
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+console.log(`Server running on port ${PORT}`);
 });
